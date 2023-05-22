5cd1a266af3985e6f98fb4d0bfeb9a8b251da565
bellabe <bella.belgarokova@hotmail.com>
2023-05-22 17:28:47 +0100

    de-lint

--- data/chains.js
+++ data/chains.js
@@ -1,26 +1,26 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const { tryAndGetEnvAsString } = require("../utils/utils");
+const { tryAndGetEnvAsString } = require('../utils/utils')
 
 const chains = {
   ethereum: {
-    scanUrl: tryAndGetEnvAsString("ETHERSCAN_API_URL"),
-    scanKey: tryAndGetEnvAsString("ETHERSCAN_API_KEY"),
+    scanUrl: tryAndGetEnvAsString('ETHERSCAN_API_URL'),
+    scanKey: tryAndGetEnvAsString('ETHERSCAN_API_KEY')
   },
   arbitrum: {
-    scanUrl: tryAndGetEnvAsString("ARBISCAN_API_URL"),
-    scanKey: tryAndGetEnvAsString("ARBISCAN_API_KEY"),
+    scanUrl: tryAndGetEnvAsString('ARBISCAN_API_URL'),
+    scanKey: tryAndGetEnvAsString('ARBISCAN_API_KEY')
   },
   binance: {
-    scanUrl: tryAndGetEnvAsString("BSC_SCAN_API_URL"),
-    scanKey: tryAndGetEnvAsString("BSC_SCAN_API_KEY"),
+    scanUrl: tryAndGetEnvAsString('BSC_SCAN_API_URL'),
+    scanKey: tryAndGetEnvAsString('BSC_SCAN_API_KEY')
   },
   polygon: {
-    scanUrl: tryAndGetEnvAsString("POLYGON_SCAN_URL"),
-    scanKey: tryAndGetEnvAsString("POLYGONSCAN_API_KEY"),
-  },
-};
+    scanUrl: tryAndGetEnvAsString('POLYGON_SCAN_URL'),
+    scanKey: tryAndGetEnvAsString('POLYGONSCAN_API_KEY')
+  }
+}
 
 module.exports = {
-  chains,
-};
+  chains
+}
